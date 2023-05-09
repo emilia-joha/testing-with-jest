cb90b600ff86e5dd41095bc4b48c7d3fa8456fdf
emilia-joha <emilia.joha@live.se>
2023-05-09 11:51:31 +0200

    Lösa error i filen

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -2,7 +2,7 @@ const { Builder, By, until } = require("selenium-webdriver");
 require("geckodriver");
 
 const fileUnderTest =
-  "file://" + __dirname.replace(/ /g, "%20") + "/../dist/index.html";
+  "file://" + __dirname.replace(/ /g, "%20") + "/../index.html";
 const defaultTimeout = 10000;
 let driver;
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
@@ -21,6 +21,7 @@ afterAll(async () => {
 
 test("The stack should be empty in the beginning", async () => {
   let stack = await driver.findElement(By.id("top_of_stack")).getText();
+  console.log(stack);
   expect(stack).toEqual("n/a");
 });
 
